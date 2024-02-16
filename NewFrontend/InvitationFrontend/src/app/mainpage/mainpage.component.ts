5513b06dc62fde808a8ede768add9fd65051c283
SeanGareth505 <seangareth505@gmail.com>
2024-02-16 18:57:08 +0200

    Mich stuff

--- NewFrontend/InvitationFrontend/src/app/mainpage/mainpage.component.ts
+++ NewFrontend/InvitationFrontend/src/app/mainpage/mainpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mainpage',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./mainpage.component.css']
 })
 export class MainpageComponent {
+  invitationForm: FormGroup = new FormGroup({});
 
+  constructor(private _fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.invitationForm = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      songRequest: [''],
+      isAccepted: [false, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    
+  }
 }
