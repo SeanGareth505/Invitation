f27eb5678ce7216e352d38f5cca00df9b6c1cae5
SeanGareth505 <seangareth505@gmail.com>
2024-02-16 13:22:38 +0200

    exmaples

--- NewFrontend/InvitationFrontend/src/app/app.module.ts
+++ NewFrontend/InvitationFrontend/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { MatCardModule } from '@angular/material/card';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule 
+    MatCardModule,
+    InputTextModule 
   ],
   providers: [ApiService, HttpClient],
   bootstrap: [AppComponent]
